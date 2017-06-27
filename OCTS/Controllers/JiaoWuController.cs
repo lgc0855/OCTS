4434ec61c7a660ca8d6db01617a499ab02457ff2
lgc0855 <867066679@qq.com>
2017-06-27 09:20:40 +0800

    导入了模板

---
+++ OCTS/Controllers/JiaoWuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OCTS.Controllers
+{
+    public class JiaoWuController : Controller
+    {
+        // GET: JiaoWu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
